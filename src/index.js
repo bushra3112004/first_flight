a3d826440eed4509020413aced632902c128fc49
bushra3112004 <pathanbushra006@gmail.com>
2024-06-03 12:20:23 -0700

    add code

--- src/index.js
+++ src/index.js
@@ -8,22 +8,24 @@ import Movie5Img from '../src/movie5.jpg';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function MovieCard({ name, city, url }) {
+function MovieCard({ name, view, url }) {
 
-    return (<div className="movie-card">
-        <h3 className="heading">hi, i am {name}</h3>
-        <p className="subheading">i am from {city}</p>
-        <img src={url} />
-
+    return (<div className="card"><div className="movie-card">
+        
+        <img src={url} alt={`${name} from ${view}`} className="img"/>
+        <h3 className="heading">Film: {name}</h3>
+        <p className="subheading">View: {view}</p>
     </div>
+</div>
 
     )
+
 }
 root.render(
     <>
-        <MovieCard name="raj" city="delhi" url="Movie1Img" />
-        <MovieCard name="priyanka" city="shimla" url="Movie2Img" />
-        <MovieCard name="arman" city="pune" url="Movie3Img" />
-        <movieCard name="pranika" city="ahmadnagar" url="Movie5Img" />
+        <MovieCard name="3 idiots" view="3.4K" url={Movie1Img} />
+        <MovieCard name="12th fail" view="1.9M" url={Movie2Img} />
+        <MovieCard name="PK" view="12.3K" url={Movie3Img} />
+        <MovieCard name="Dear Zindagi" view="99.0K" url={Movie5Img} />
     </>
 )
